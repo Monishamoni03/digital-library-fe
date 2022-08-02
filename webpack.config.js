43fc587ac918026dfed04709b1f3ca720a574303
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-08-02 11:32:35 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,6 @@ module.exports = {
         open: true,
         historyApiFallback: true,
         liveReload: false,
-        hotOnly: false,
-       inline: true,
     },
     resolve: {
         extensions: [".js",".css",".jsx",".json",".jpg",".png",".jpeg"]
